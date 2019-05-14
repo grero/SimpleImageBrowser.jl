76f66530594ecfc32e0f98c2ff7cd62916e7ad41
roger <roger.herikstad@gmail.com>
2019-05-14 15:37:59 +0800

    Adds tests and travis config

---
+++ test/runtests.jl
@@ -0,0 +1,20 @@
+using SimpleImageBrowser
+using Test
+
+@testset "Basic" begin
+    tdir = tempdir()
+    cd(tdir) do
+        #grab some test images
+        download("https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Pears.jpg/256px-Pears.jpg", "pear.jpg")
+        download("https://upload.wikimedia.org/wikipedia/commons/thumb/f/f4/Honeycrisp.jpg/512px-Honeycrisp.jpg", "apple.jpg")
+        img_files = ["apple.jpg","pear.jpg"]
+        SimpleImageBrowser.generate_webpage(img_files)
+        @test isfile("app/images/apple.jpg")
+        @test isfile("app/images/pear.jpg")
+        @test isfile("app/index.html")
+        txt = readchomp(open("app/index.html"))
+        @test txt == "<html><head><title>Simple Image Browoser</title></head>\n<body>\n<img src=images/apple.jpg width=256.0 height=256.0 border=1px>\n<img src=images/pear.jpg width=128.0 height=195.0 border=1px>\n</body>\n</html>"
+        #cleanup
+        rm("app", recursive=true, force=true)
+    end
+end
